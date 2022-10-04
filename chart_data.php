0ccfbbaa063629c1e873e8895b061ec4abcf9498
SylwesterSzczepanski <73478345+SylwesterSzczepanski@users.noreply.github.com>
2022-10-04 18:31:23 +0200

    Create chart_data.php

---
+++ chart_data.php
@@ -0,0 +1,50 @@
+<?php
+//address of the server where db is installed
+$servername = "your_server";
+
+//username to connect to the db
+$username = "your_user_name";
+
+//password to connect to the db
+$password = "your_password";
+
+//name of the db under which the table is created
+$dbName = "your_DB_name";
+
+//establishing the connection to the db.
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+//checking if there were any error during the last connection attempt
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//the SQL query to be executed
+$query = "SELECT value1,reading_time FROM your_table";
+
+//storing the result of the executed query
+$result = $conn->query($query);
+
+//initialize the array to store the processed data
+$jsonArray = array();
+
+//check if there is any data returned by the SQL Query
+if ($result->num_rows > 0) {
+  //Converting the results into an associative array
+  while($row = $result->fetch_assoc()) {
+    $jsonArrayItem = array();
+    $jsonArrayItem['label'] = $row['reading_time'];
+    $jsonArrayItem['value'] = $row['value1'];
+    //append the above created object into the main array.
+    array_push($jsonArray, $jsonArrayItem);
+  }
+}
+
+//Closing the connection to DB
+$conn->close();
+
+//set the response content type as JSON
+header('Content-type: application/json');
+//output the return value of json encode using the echo function. 
+echo json_encode($jsonArray);
+?>
